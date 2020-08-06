6122757d01645b6603bae38e76d5506863d04ce8
asato <a-ohkubo@cmind-co.jp>
2020-08-06 11:14:19 +0900

    3-4 finish

---
+++ app/Http/Controllers/Admin/ProfileController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class ProfileController extends Controller
+{
+    public function add()
+    {
+        return viwe('admin.profile.create');
+    }
+
+    public function create()
+    {
+        return redirect('admin/frofile/create');
+    }
+
+    public function edit()
+    {
+        return view('admin.profile.edit');
+    }
+
+    public function update()
+    {
+        return redirect('admin/profile/edit');
+    }
+}
